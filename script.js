5add308fd811837912dd0d2a6ad2bcb5e5f4dd38
rujnh <aniruj.w@kkumail.com>
2024-01-08 11:05:03 +0700

    แก้ไขค้นหา

---
+++ script.js
@@ -0,0 +1,10 @@
+//  // Add event listener for the dropdown item clicks
+//  $('.color-item').click(function() {
+//     // Handle the click for สีทั้งหมด
+//     console.log('Show สีแดง, น้ำเงิน, เขียว');
+// });
+
+// $('.price-item').click(function() {
+//     // Handle the click for ราคา
+//     console.log('Show มากไปน้อย, น้อยไปมาก');
+// });
